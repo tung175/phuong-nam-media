42717a0d29085a93bbe687b6d59d9bbec8c3bb6f
Kyutung <tungduong@vnpt.vn>
2025-10-23 14:04:20 +0700

    fistcommit

---
+++ lemondigital.vn/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from lemondigital.vn/xmlrpc.php?rsd by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 23 Oct 2025 03:29:26 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://127.0.0.1/?rsd"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://127.0.0.1/?rsd"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from lemondigital.vn/xmlrpc.php?rsd by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 23 Oct 2025 03:29:26 GMT -->
+</HTML>
